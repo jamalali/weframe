a871c084e11eadbefaa8299408ac829792157c89
Jamal Ali <jamalali81@gmail.com>
2019-04-07 23:48:49 +0100

    now getting price for mount (single) as well as mould and glass

--- app/Http/Controllers/API/PriceController.php
+++ app/Http/Controllers/API/PriceController.php
@@ -26,14 +26,95 @@ class PriceController extends Controller
 		$glass_size_height		= $request->input('glass_size_height');
 		
 		$mould_price = $this->getMouldPrice($glass_size_width, $glass_size_height, $mould_cost, $mould_width, $settings);
-		
 		$glass_price = $this->getGlassPrice($glass_size_width, $glass_size_height, $settings);
+		$mount_price = $this->getMountPrice($glass_size_width, $glass_size_height, $mount, $settings);
+		
+		$total = $mould_price + $glass_price + $mount_price;
 		
 		return response()->json([
-			'mould_price' => $mould_price,
-			'glass_price' => $glass_price
+			'mould_price'	=> number_format($mould_price, 2),
+			'glass_price'	=> number_format($glass_price / 100, 2),
+			'mount_price'	=> number_format($mount_price / 100, 2),
+			'total'			=> number_format($total / 100, 2)
 		]);
     }
+	
+	private function getMountPrice($frame_width, $frame_height, $mount, $settings) {
+		
+		$wastage	= $settings['mount_board_wastage'];
+		$markup		= $settings['mount_board_markup'];
+		
+		$frame = [
+			'width'		=> $frame_width,
+			'height'	=> $frame_height
+		];
+		
+		$jumbo_peice = [
+			'width'		=> $settings['jumbo_mount_board']['width'],
+			'height'	=> $settings['jumbo_mount_board']['height'],
+			'price'		=> $settings['jumbo_mount_board']['price']
+		];
+		
+		$full_peice = [
+			'width'		=> $settings['standard_mount_board']['width'],
+			'height'	=> $settings['standard_mount_board']['height'],
+			'price'		=> $settings['standard_mount_board']['price']
+		];
+		
+		$half_peice = $this->cutInHalf($full_peice);
+		$quarter_peice = $this->cutInHalf($half_peice);
+		
+		if ($this->fits($frame, $quarter_peice))	{ $mount_price = $quarter_peice['price'];} 
+		else if ($this->fits($frame, $half_peice))	{ $mount_price = $half_peice['price'];}
+		else if ($this->fits($frame, $full_peice))	{ $mount_price = $full_peice['price'];}
+		else if ($this->fits($frame, $jumbo_peice))	{ $mount_price = $jumbo_peice['price'];}
+		
+		// Add the percentage wastage
+		$mount_price = $mount_price + ($mount_price / 100 * $wastage);
+		
+		// Add the percentage markup
+		$mount_price = $mount_price + ($mount_price / 100 * $markup);
+		
+		return $mount_price;
+	}
+	
+	private function getGlassPrice($frame_width, $frame_height, $settings) {
+		
+		$wastage	= $settings['glass_wastage'];
+		$markup		= $settings['glass_markup'];
+		
+		$frame = [
+			'width'		=> $frame_width,
+			'height'	=> $frame_height
+		];
+		
+		// The glass sizes i.e. full, half & quarter		
+		$full_peice = [
+			'width'		=> $settings['standard_float_glass']['width'],
+			'height'	=> $settings['standard_float_glass']['height'],
+			'price'		=> $settings['standard_float_glass']['price']
+		];
+		
+		// Cut the full size in her half to get the half size
+		$half_peice		= $this->cutInHalf($full_peice);
+		
+		// Cut the half size in her half to get the quarter size
+		$quarter_peice	= $this->cutInHalf($half_peice);
+		
+		// Now check which size glass peice the customers frame size fits into
+		// and use that price
+		if ($this->fits($frame, $quarter_peice))	{ $glass_price = $quarter_peice['price'];} 
+		else if ($this->fits($frame, $half_peice))	{ $glass_price = $half_peice['price'];}
+		else if ($this->fits($frame, $full_peice))	{ $glass_price = $full_peice['price'];}
+		
+		// Add the percentage wastage
+		$glass_price = $glass_price + ($glass_price / 100 * $wastage);
+		
+		// Add the percentage markup
+		$glass_price = $glass_price + ($glass_price / 100 * $markup);
+		
+		return $glass_price;
+	}
 	
 	private function getMouldPrice($glass_width, $glass_height, $mould_cost, $mould_width, $settings) {
 		
@@ -48,56 +129,27 @@ class PriceController extends Controller
 		return $total_mould_cost_plus_wastage;
 	}
 	
-	private function getGlassPrice($frame_width, $frame_height, $settings) {
+	private function fits($frame, $peice) {
+		return $frame['width'] <= $peice['width'] && $frame['height'] <= $peice['height'];
+	}
+
+	private function cutInHalf($peice) {
 		
-		$wastage	= $settings['glass_wastage'];
-		$markup		= $settings['glass_markup'];
-		
-		// The glass sizes i.e. full, half & quarter
-		$width_full		= $settings['standard_float_glass']['width'];
-		$height_full	= $settings['standard_float_glass']['height'];
-		$price_full		= $settings['standard_float_glass']['price'];
-		
-		// Cut the full size in her half to get the half size
 		// We want to cut the long side not the short side
-		if ($width_full > $height_full) {
-			$width_half		= $width_full / 2;
-			$height_half	= $height_full;
-			$price_half		= $price_full / 2;
+		if ($peice['width'] > $peice['height']) {
+			$width_half		= $peice['width'] / 2;
+			$height_half	= $peice['height'];
+			$price_half		= $peice['price'] / 2;
 		} else {
-			$width_half		= $width_full;
-			$height_half	= $height_full /2;
-			$price_half		= $price_full / 2;
+			$width_half		= $peice['width'];
+			$height_half	= $peice['height'] /2;
+			$price_half		= $peice['price'] / 2;
 		}
 		
-		// Cut the half size in her half to get the quarter size
-		// Again, we want to cut the long side not the short side
-		if ($width_half > $height_half) {
-			$width_quarter		= $width_half / 2;
-			$height_quarter		= $height_half;
-			$price_quarter		= $price_half / 2;
-		} else {
-			$width_quarter		= $width_half;
-			$height_quarter		= $height_half / 2;
-			$price_quarter		= $price_half / 2;
-		}
-		
-		// Now check which size glass peice the customers frame size fits into
-		// and use that price
-		if ($frame_width <= $width_quarter && $frame_height <= $height_quarter) {
-			$glass_price = $price_quarter;
-		} else if ($frame_width <= $width_half && $frame_height <= $height_half) {
-			$glass_price = $price_half;
-		} else if ($frame_width <= $width_full && $frame_height <= $height_full) {
-			$glass_price = $price_full;
-		}
-		
-		// Add the percentage wastage
-		$glass_price = $glass_price + ($glass_price / 100 * $wastage);
-		
-		// Add the percentage markup
-		$glass_price = $glass_price + ($glass_price / 100 * $markup);
-		
-		return $glass_price;
+		return [
+			'width' => $width_half,
+			'height' => $height_half,
+			'price' => $price_half
+		];
 	}
 }
